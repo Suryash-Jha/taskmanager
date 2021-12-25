8e2c6e486a6af8b97aefe18d2fbed0b11ab111e1
Suryash-Jha <joblesscoder2468@gmail.com>
2021-12-25 17:20:17 +0530

    Completed and report functionality added & project completed

--- Project.cpp
+++ Project.cpp
@@ -9,11 +9,11 @@ m["add"]= 1;
 m["ls"]=2;
 m["del"]=3;
 m["done"]=4;
-m["help"]= 5;
-m["report"]=6;
+m["report"]=5;
 
 fstream myfile;
   multimap<int, string> taskXprior;
+  multimap<int, string> completed;
   myfile.open("file.txt", ios::in);
   if(myfile.is_open()){
     string line;
@@ -50,7 +50,7 @@ int command= m[argv[1]];
           if(myfile.is_open())
           {
           	for(auto it: taskXprior){
-               cout<<it.first<<" "<<it.second<<endl;
+               // cout<<it.first<<" "<<it.second<<endl;
           		myfile<<it.first<<" "<<it.second<<endl;
           	}
           	myfile.close();
@@ -68,7 +68,7 @@ int command= m[argv[1]];
             	while(getline(myfile, line)){
             		auto priority = stoi(line.substr(0, line.find(' ')));
  					auto task= line.substr(line.find(' ')+1, line.length());
- 					cout<<count<<".) "<<task<<"["<<priority<<"]"<<endl;
+ 					cout<<count<<". "<<task<<"["<<priority<<"]"<<endl;
  					count++;
             	}
             	myfile.close();
@@ -80,7 +80,6 @@ int command= m[argv[1]];
     case 3:
           {
           	auto index= stoi(argv[2]);
-          	// cout<<typeid(index).name()<<endl;
           	auto it= taskXprior.begin();
           	for(int i=0; i<index-1; i++)
           	{
@@ -97,8 +96,64 @@ int command= m[argv[1]];
 
           }
           break;
+    
+    case 4:
+          {
+          	auto index= stoi(argv[2]);
+          	int num;
+          	string str;
+          	{
+          		auto it= taskXprior.begin();
+          	for(int i=0; i<index-1; i++)
+          	{
+          		it++;
+          	}
 
+          	num= it->first;
+          	str= it->second;
+          	taskXprior.erase(it);
+          	myfile.open("file.txt", ios::out);
+    	   {
+     		for(auto it: taskXprior){
+      		myfile<<it.first<<" "<<it.second<<endl;
+    		}
+    		myfile.close(); 
+    		}
+          }
+
+          	// completed.insert({num, str});
+          	myfile.open("complete.txt", ios::app);
+          	if(myfile.is_open()){
+                
+                	myfile<<num<<" "<<str<<endl;
+      
+                myfile.close();
+          	}
+          }
+          break;
    	case 5: 
+          {
+          	int pendingt=0, completedt=0;
+            for(auto it: taskXprior)
+            {
+            	pendingt++;
+            }
+            myfile.open("complete.txt", ios::in);
+            if(myfile.is_open()){
+            	string s;
+            	while(getline(myfile, s))
+            	{
+            		completedt++;
+            	}
+            	myfile.close();
+            }
+          	cout<<"Pending: "<<pendingt<<endl;
+          	cout<<"Completed: "<<completedt<<endl;
+          	
+          }
+          break;
+
+   	default:
    	    cout<<"Usage :-";
 		cout<<"	\n	$ ./task add 2 hello world    # Add a new item with priority 2 and text \"hello world\" to the list";
 		cout<<"	\n	$ ./task ls                   # Show incomplete priority list items sorted by priority in ascending order";
@@ -107,9 +162,6 @@ int command= m[argv[1]];
 		cout<<"	\n	$ ./task help                 # Show usage";
 		cout<<"	\n	$ ./task report               # Statistics";
    	      break;
-
-   	default:
-   	      cout<<"\n Wrong input";
    }
 
 
